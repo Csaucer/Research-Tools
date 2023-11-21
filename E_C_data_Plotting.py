e505c4e2be18eaaee466caa4dd6e3c6291db929d
Csaucer <116199412+Csaucer@users.noreply.github.com>
2023-11-21 07:53:14 -0600

    Added E_C data plotting (developed for May-July Bear Spring EC data) and added the MFIT data processing tool

---
+++ E_C_data_Plotting.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 16:23:20 2023
+
+@author: csouc
+"""
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import os
+# %%
+# set the current working directory
+path = 'C:/Campbellsci/PC400'
+
+os.chdir(path)
+
+#list the names of the files in the directory
+os.listdir()
+
+#select the name of the file that you want to be read into the plotter
+filename = 'Bear_Spring_EC_TEMP_UMN_Table1.dat'
+
+def reader(filename):
+    with open(filename, "r") as my_file:
+        Contents = my_file.read()
+    return Contents
+
+# %% call the reading function
+# Assuming the provided data is stored in a file named 'data.dat'
+# You might need to adjust the delimiter based on the actual format of your .dat file
+data = pd.read_csv(filename, delimiter=',', skiprows=1)
+
+# Extract the desired columns
+selected_columns = ['TIMESTAMP', 'Cond_Avg', 'Ct_Avg', 'Temp_C_Avg']
+selected_data = data[selected_columns]
+selected_data = selected_data.dropna()
+
+# Print the selected data
+print(selected_data)
+
+#%%
+# plot the selected data
+
+x = selected_data['TIMESTAMP']
+
+
+y = selected_data['Cond_Avg']
+
+x = x[2:]
+
+days = np.arange(0, len(x)) * 15 * 0.000694444
+
+y = y[2:]
+y = y.dropna()
+y = y.to_numpy()
+
+
+y_float = []
+for i in range(len(y)):
+    y_float.append(float(y[i]))
+    
+# Plot the data
+fig = plt.figure()
+
+# Using a scatter plot with marker="o" and specifying the tick values
+plt.scatter(days, y_float, marker="o")
+
+# Set y tick values and labels
+plt.yticks([0, 0.5, 1, 1.5, 2], ['0', '0.5', '1', '1.5', '2'])
+plt.ylabel('Conductivity (T corrected) [mS/cm]')
+plt.xlabel('Elapsed Days')
+plt.title('Bear Spring Conductivity, May to August 2023')
+# Other plot customizations...
+plt.savefig('May_to_August_Conductivity_Bear_Spring.png', dpi=800)
+plt.show()
+
+
+
+
+
+
+
---
+++ MFIT_Data_processing.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 15:56:21 2023
+
+@author: csouc
+"""
+
+"""This script processes decay corredted data (using data_process.py) for import
+into MFIT-BTC for transport process analysis, and then is used to take the output
+data and plot it against the observational data"""
+
+#import the necessary package dependencies
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+#import the data_process python file and run it to decay correct the data for MFIT import
+os.chdir('G:/My Drive/Core Flooding Project/UW Madison/Rad_BTC')
+#import the data process module
+import data_process
+
+# %% #decay correct the raw rad data
+#run the decay correction with the data_process module from Chris Zahasky
+filename = r"G:\My Drive\Core Flooding Project\UW Madison\Rad_BTC\Rad_data\11MAY2023_mg1771782c_PET_rad.csv"
+s = data_process.sensors(filename, isotope = 'f18')
+#check the starting experiment times, plot them and establish the x values
+s.experiment_time_extraction(n=3, plot_check='yes')
+#save the experiment starting times (rad 2) for each of the experiments
+exp_start = s.eidx
+
+exp_start[2] = 8370 #the 3rd experiment has super low radioactivity, so the auto detect doesnt work well
+#manually establish the 3rd data point here
+exp_start = np.insert(exp_start, 3, len(s.SR3_dc))
+print(exp_start) #check these values to see that they match the plots
+# %%
+#plot the data from the 3rd radioactivity sensor to see the total SR3 data collected
+x = np.linspace(0, len(s.SR3_dc)-1,num=len(s.SR3_dc))
+plt.plot(x, s.SR3_dc)
+
+#use the start time data to export each of the experiments as .csv files for MFIT
+#set the number of experiments
+n=3
+#name the core and the before/after indicator
+ident = 'C2_pre'
+#run a loop to extract the appropriate data
+for i in range(len(s.SR3_dc)):
+    if i <= 2:
+        t_min = exp_start[i]
+        t_max = exp_start[i+1]
+        arr = s.SR3_dc[t_min:t_max]
+        time = np.arange(arr.shape[0])
+        arr = np.column_stack((time, arr))# add a time array
+        weight = np.ones_like(time)
+        arr=np.column_stack((arr, weight))
+        #save the array as a .csv file
+        fname = f'\\{ident}_exp{i+1}.csv'
+        loc = os.getcwd()
+        np.savetxt(loc+fname, arr, delimiter=',')
+        #print(np.max(arr))
+        
+    else:
+        break
+# %%
+#test the above to see what the .csv data looks like
+import pandas as pd
+y = pd.read_csv('C1_pre_exp1.csv')
+x = np.linspace(0, len(y)-1, len(y))
+plt.plot(x,y)
+#%%
+#integrate under the curve to get the total tracer mass to input in MFIT
+mass = np.trapz(y.iloc[:, 1])
+# %%
+#MFIT doesn't work with data values that are negative or 0, setting the lower limit
+#of the data to 0.0000001
+#for i in range(len(data)):
+#    if data[i,1] <=  0.0000001:
+#        data[i,1] =  0.0000001
+#plot a test plot of the raw observational data
+#fig =plt.subplot()
+#plt.plot(data[:,0], data[:,1])
+#plt.title('Timeseries plot of Rad BTC data')
+#plt.xlabel('Time in Seconds')
+#plt.ylabel('Radioconcentration [mCi/mL]')
+
