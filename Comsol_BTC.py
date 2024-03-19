010450093107bbec541fad6fdb7d09eb3564aaec
Csaucer <116199412+Csaucer@users.noreply.github.com>
2024-03-19 16:38:45 -0500

    Saved V5 version of Comsol_BTC.py

--- Comsol_BTC.py
+++ Comsol_BTC.py
@@ -11,7 +11,7 @@ import os
 import pandas as pd
 from matplotlib import pyplot as plt
 #change directory 
-directory = 'D:\COMSOL_Files\COMSOL_BTC_DATA\COMSOL_concentration_distributions' #desktop directory
+directory = r'C:\Users\csouc\Core Flooding Laptop\COMSOL Simulations\COMSOL_BTC_data\V5' #desktop directory
 #directory = r'C:\Users\csouc\Core Flooding Laptop\COMSOL Simulations\COMSOL_BTC_data' #laptop directory
 os.chdir(directory)
 # %%
@@ -111,12 +111,15 @@ savefig = False #flag for saving the figures
 #6mL/min
 florate = 6
 #load BTC data
-BTC =  pd.read_csv('total_outlet_C_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-frac_BTC =  pd.read_csv('fracture_outlet_C_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-chan_BTC =  pd.read_csv('channel_outlet_C_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
+#BTC =  pd.read_csv('total_outlet_C_6mL.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
+frac_BTC =  pd.read_csv('6mL_min_frac_out.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)' ])
+chan_BTC =  pd.read_csv('6mL_min_chan_out.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)' ])
+#get the total discharge
+BTC = frac_BTC + chan_BTC
+
 x = BTC.iloc[:,0] #get the time array in seconds
 fig1, axs = plt.subplots(3,1 , figsize=(12,9))
-fig1.suptitle('COMSOL BTC; 6mL/min', fontsize=20)
+fig1.suptitle('COMSOL BTC; 6 mL/min', fontsize=20)
 xlabel = 'Time [s]'
 ylabel = 'Solute Amount [mols]' #the y axis is in mols as this is a volume integration
 
@@ -153,7 +156,7 @@ if savefig:
     
 ####### SEMILOG BTC's
 fig2, axs2 = plt.subplots(3,1, figsize=(12,9))
-fig2.suptitle('COMSOL BTC; 6mL/min Semilog', fontsize=20)
+fig2.suptitle('COMSOL BTC; 6 mL/min Semilog', fontsize=20)
 #do the same thing as above but with semilog y plots
 #plot the overall BTC
 axs2[0].plot(x, BTC.iloc[:,1], color='Orange')
@@ -161,6 +164,7 @@ axs2[0].set_title('Total BTC', fontsize=16)
 axs2[0].set_xlabel(xlabel, fontsize=14)
 axs2[0].set_ylabel(ylabel,fontsize=14)
 axs2[0].semilogy()
+axs2[0].set_ylim(10E-13, 10E-8)
 
 #plot the fracture component of the BTC
 axs2[1].plot(x, frac_BTC.iloc[:,1], color='Orange')
@@ -168,6 +172,7 @@ axs2[1].set_title('Fracture Outlet BTC', fontsize= 16)
 axs2[1].set_xlabel(xlabel, fontsize=14)
 axs2[1].set_ylabel(ylabel, fontsize=14)
 axs2[1].semilogy()
+axs2[1].set_ylim(10E-15, 10E-9)
 
 #plot the channel component of the BTC
 axs2[2].plot(x, chan_BTC.iloc[:,1], color='Orange')
@@ -175,6 +180,7 @@ axs2[2].set_title('Channel Outlet BTC', fontsize=16)
 axs2[2].set_xlabel(xlabel, fontsize=14)
 axs2[2].set_ylabel(ylabel, fontsize=14)
 axs2[2].semilogy()
+axs2[2].set_ylim(10E-13, 10E-8)
 
 plt.subplots_adjust(hspace=0.5, top=0.9) # Increase vertical spacing between subplots
 
@@ -184,84 +190,6 @@ if savefig:
     os.chdir(savedir)
     plt.savefig(f'{florate}_mL_min_semilog_COMSOL_BTCs.png')
     os.chdir(datadir)
-
-#%%    
-#get the data from the 0.09 and 6mL/min plotted on the same graph, normalized
-#by time
-#6mL/min
-BTC_6 =  pd.read_csv('total_outlet_C_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-frac_BTC_6 =  pd.read_csv('fracture_outlet_C_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-chan_BTC_6 =  pd.read_csv('channel_outlet_C_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-x6 = BTC_6.iloc[:,0]
-tstep6 = 0.05
-#0.09mL/min
-BTC_009 =  pd.read_csv('total_outlet_C_009mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-frac_BTC_009 =  pd.read_csv('fracture_outlet_C_009mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-chan_BTC_009 =  pd.read_csv('channel_outlet_C_009mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-x009 = BTC_009.iloc[:,0]
-tstep009 = 0.5
-
- 
-#Total BTCs
-#normalize the data by the peak time
-#get the time of the data peaks
-t1 = np.argmax(BTC_6.iloc[:,1]) * tstep6
-t2 = np.argmax(BTC_009.iloc[:,1]) *tstep009
-
-#x6 = x6/t1
-#x009 = x009/t2
-#plot the normalized breakthrough curves (normalized by peak arrival time)
-#fig, ax = plt.subplots(figsize=(12,9), dpi=800)
-#plt.plot(x6, BTC_6.iloc[:,1])
-#plt.plot(x009, BTC_009.iloc[:,1])
-#plt.semilogy()
-
-#plot normalized by VI
-x6 = BTC_6.iloc[:,0]
-x009 = BTC_009.iloc[:,0] #reset to the original x axes
-
-
-x6 = x6 * 100
-x009 = x009 * 1.5
-#total BTC
-fig, ax = plt.subplots(figsize=(12,9), dpi=800)
-plt.plot(x6, BTC_6.iloc[:,1], label='6mL/min')
-plt.plot(x009, BTC_009.iloc[:,1], label='0.09mL/min')
-plt.semilogy()
-plt.title('VI Norm. BTC, Total Solute Discharge', fontsize=20)
-plt.xlabel('Vol. Injected (mL)', fontsize=18)
-plt.ylabel('Quantity of solute [mol]', fontsize=18)
-plt.legend(loc='upper right', fontsize=18)
-plt.ylim((10E-13,10E-7))
-plt.tick_params(axis='x', which='major', labelsize=16) # x-axis tick size
-plt.tick_params(axis='y', which='major', labelsize=16) # y-axis tick size
-
-#fractures
-fig, ax = plt.subplots(figsize=(12,9), dpi=800)
-plt.plot(x6, frac_BTC_6.iloc[:,1], label='6mL/min')
-plt.plot(x009, frac_BTC_009.iloc[:,1], label='0.09mL/min')
-plt.title('VI Norm. BTC, Fracture Solute Discharge', fontsize=20)
-plt.xlabel('Vol. Injected (mL)', fontsize=18)
-plt.ylabel('Quantity of solute [mol]', fontsize=18)
-plt.semilogy()
-plt.legend(loc='upper right', fontsize=18)
-plt.ylim((10E-13,10E-9))
-plt.tick_params(axis='x', which='major', labelsize=16) # x-axis tick size
-plt.tick_params(axis='y', which='major', labelsize=16) # y-axis tick size
-
-#channels
-fig, ax = plt.subplots(figsize=(12,9), dpi=800)
-plt.plot(x6, chan_BTC_6.iloc[:,1], label='6mL/min')
-plt.plot(x009, chan_BTC_009.iloc[:,1], label='0.09mL/min')
-plt.title('VI Norm. BTC, Channel Solute Discharge', fontsize=20)
-plt.xlabel('Vol. Injected (mL)', fontsize=18)
-plt.ylabel('Quantity of solute [mol]', fontsize=18)
-plt.legend(loc='upper right', fontsize=18)
-plt.semilogy()
-plt.ylim((10E-13,10E-7))
-plt.tick_params(axis='x', which='major', labelsize=16) # x-axis tick size
-plt.tick_params(axis='y', which='major', labelsize=16) # y-axis tick size
-
 # %%
 '''Using a similar process as outlined below with the % of total groundwater approach,
 establish the % of total discharged concentration through the fracture, and the channel
@@ -280,8 +208,8 @@ chan_eff_per = (chan_BTC.iloc[:,1]/BTC.iloc[:,1])*100
 
 #Now plot the % discharged throughout time
 fig, ax = plt.subplots(figsize=(12,9), dpi=800)
-plt.plot(x, chan_eff_per, label ='% discharge, channel', linewidth=3)
 plt.plot(x, frac_eff_per, label= '% discharge, fracture', linewidth=3)
+plt.plot(x, chan_eff_per, label ='% discharge, channel', linewidth=3)
 plt.legend(loc='center right', fontsize=16)
 plt.ylabel('% of Total Tracer in System', fontsize=18)
 plt.xlabel('Time [s]', fontsize=18)
@@ -293,9 +221,6 @@ os.chdir(savedir)
 #plt.savefig('solute_discharge_6mL.png')
 os.chdir(datadir)
 
-
-
-
 # %%
 # I may have established a better way to integrate the concentration data spatially
 #in COMSOL. Plotting that data here
@@ -306,10 +231,10 @@ THIS IS NOT BTC DATA. That requires a surface (2D) integration at the outlet
 of the system"""
 savefig = False #flag operator for saving the figure as a .png
 florate = 6
-
-frac = pd.read_csv('fracture_C_6mL_integration_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-chan = pd.read_csv('Channel_C_6mL_integration_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-inlet = pd.read_csv('inlet_block_C_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
+#6mL/min
+frac = pd.read_csv('fracture_C_6mL_integration.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
+chan = pd.read_csv('Channel_C_6mL_integration.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
+inlet = pd.read_csv('inlet_block_C_6mL.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
 
 #plot the total amount of solute in the fracture and the total solute in the channel, and the total solute overall (sum)
 #the total amount of solute in the domain is going to change over time as solute
@@ -337,30 +262,18 @@ if savefig:
     plt.savefig(f'{florate}mL_min_solute_distribution')
     os.chdir(datadir)
     
-#%%
-#commented out as I have already done this below
-#'''Instead of plotting solute distribution as a % of the total, plot it as
-# mols (no conversion to %)'''
-#fig, ax = plt.subplots(figsize=(12,9), dpi=800)
-#plt.plot(x, chan.iloc[:,1], label = 'Channel' ,linewidth = 3)
-#plt.plot(x, frac.iloc[:,1], label = 'Fracture', linewidth = 3)
-#plt.title('6mL/min Solute Distribution', fontsize=20)
-#plt.xlabel('Time [s]', fontsize=18)
-#plt.ylabel('Tracer [mol]', fontsize=18)
-#plt.tick_params(axis='x', which='major', labelsize=16) # x-axis tick size
-#plt.tick_params(axis='y', which='major', labelsize=16) # y-axis tick size
-#plt.legend(loc = 'upper right', fontsize=16)
-#plt.semilogy()
+    
 # %%
 #Regenerating all of these plots but using mass (or amount, mols) instead of % of tracer or % discharge
+
+
 #6mL/min
 #Tracer distribution throughout the domain
-savefig = False
-florate = '009'
-frac = pd.read_csv('fracture_C_009mL_integration_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-chan = pd.read_csv('Channel_C_009mL_integration_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
-inlet = pd.read_csv('inlet_block_C_009mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)'])
+frac = pd.read_csv('009mL_min_frac_vol.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)' ])
+chan = pd.read_csv('009mL_min_chan_vol.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Concentration (mol)' ])
 
+
+# %%
 #fix the weird issue with the diffusion out from the initial concentration box
 frac.iloc[0,1] = 0
 chan.iloc[0,1] = 0
@@ -369,58 +282,9 @@ fig, ax = plt.subplots(figsize=(12,9), dpi=800)
 plt.plot(x, chan.iloc[:,1], label = 'Channel')
 plt.plot(x, frac.iloc[:,1], label = 'Fracture')
 ax.semilogy()
-plt.title(f'{florate}mL/min Tracer Distribution, Semilog', fontsize=24)
+plt.title('Tracer Distribution Throughout Fracture 0.09, Semilog', fontsize=24)
 plt.tick_params(axis='x', which='major', labelsize=16) # x-axis tick size
 plt.tick_params(axis='y', which='major', labelsize=16) # y-axis tick size
 plt.ylabel('Amount of Tracer [mol]', fontsize=18)
 plt.xlabel('Time [s]', fontsize=18)
-plt.legend(loc='upper right', fontsize=16)
-if savefig:
-    datadir = os.getcwd()
-    savedir = 'D:\COMSOL_Files\COMSOL_BTC_Plots'
-    os.chdir(savedir)
-    plt.savefig(f'{florate}mL_min_solute_dist_mols_semilog')
-    os.chdir(datadir) 
-
-#%%
-#The discharge for both simulations are constant throuhgout, so integrate and
-#get the % discharge from the fracture and the channel out of the total discharge
-
-#6mL/min
-#import the datafiles
-dtot =  pd.read_csv('total_discharge_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Velocity magnitude (m^3/s)'])
-dfrac = pd.read_csv('fracture_discharge_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Velocity magnitude (m^3/s)'])
-dchan = pd.read_csv('channel_discharge_6mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Velocity magnitude (m^3/s)'])
-#calculate the total discharge across the entire outlet
-dis_tot = np.trapz(dtot.iloc[:,1])
-#calculate the discharge from the fracture only
-dis_frac = np.trapz(dfrac.iloc[:,1])
-#calculate the discharge from the channel only
-dis_chan = np.trapz(dchan.iloc[:,1])
-
-#fracture discharge %
-frac_per = (dis_frac/dis_tot)*100
-chan_per = (dis_chan/dis_tot)*100
-#print the data
-print(f'6mL/min fracture contribution to discharge: {frac_per}')
-print(f'6mL/min channel contribution to discharge: {chan_per}')
-
-
-#0.09mL/min
-dtot =  pd.read_csv('total_discharge_009mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Velocity magnitude (m^3/s)'])
-dfrac = pd.read_csv('fracture_discharge_009mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Velocity magnitude (m^3/s)'])
-dchan = pd.read_csv('channel_discharge_009mL_V4.txt', skiprows=5, delim_whitespace=True, names=['Time', 'Velocity magnitude (m^3/s)'])
-#calculate the total discharge across the entire outlet
-dis_tot = np.trapz(dtot.iloc[:,1])
-#calculate the discharge from the fracture only
-dis_frac = np.trapz(dfrac.iloc[:,1])
-#calculate the discharge from the channel only
-dis_chan = np.trapz(dchan.iloc[:,1])
-
-#fracture discharge %
-frac_per = (dis_frac/dis_tot)*100
-chan_per = (dis_chan/dis_tot)*100
-#print the data
-print(f'0.09mL/min fracture contribution to discharge: {frac_per}')
-print(f'0.09mL/min channel contribution to discharge: {chan_per}')
-
+plt.legend(loc='upper right', fontsize = 16)
